fb9d2a9b7da1b0425cb60614f16e2760ff63a576
DevLeonir <juniorjoga102011@hotmail.com>
2025-06-07 17:52:55 -0300

    Exemplos com operadores lógicos AND, OR e NOT

---
+++ unidade_01/exemplo03.py
@@ -0,0 +1,7 @@
+a = 5
+b = 6
+c = a
+print("variável a antes de ter um novo valor atribuído ", a)
+a = "texto"
+print("valor atribuido para a variável c ", c)
+print("agora o valor armazenado na variável a é um tipo texto ", a)
