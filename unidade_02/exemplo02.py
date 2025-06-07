8869d1f5241dbf4570506f2cc698ce5501b1cddf
DevLeonir <juniorjoga102011@hotmail.com>
2025-06-07 18:27:44 -0300

    Exemplo de operações de comparação

---
+++ unidade_02/exemplo02.py
@@ -0,0 +1,26 @@
+# Operador para comparar valores iguais
+a = 5
+b = 5
+
+if a == b:
+    print("a é igual a b")
+    
+# Operador para comparar valores diferentes
+a = 5
+b = 2
+
+if a != b:
+    print("a é diferente de b")
+    
+# Operador para comparar se o valor do termo a é maior que o termo b
+a = 5
+b = 2
+
+if  a > b:
+    print("a é maior que b")
+    
+# Operador para comparar se o valor do termo a é menor que o termo b
+a = 1
+b = 2
+if a < b:
+    print("a é menor que b")
