f8898fc39ecb88e7ccad4bf9bc5e98eeb5b6eff3
DevLeonir <juniorjoga102011@hotmail.com>
2025-06-07 18:11:13 -0300

    Recebe e exibe nome, idade e peso do usuário

---
+++ unidade_01/exemplo05.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+peso = float(input("Digite seu peso: "))
+
+print("O nome digitado é", nome)
+print("A idade digitada é", idade)
+print("O peso digitado é", peso)
